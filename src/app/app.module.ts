b7d6d410453ac5fd27d20f9eaad1a4e4dfd62846
Dawid Lubera <dlubera@crodu.com>
2018-05-11 21:40:14 +0200

    Geolocalization and reverse localization testing, bottom nav changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,9 @@ import { TabsPage } from '../pages/tabs/tabs';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
+import { Geolocation } from '@ionic-native/geolocation';
+import { NativeGeocoder } from '@ionic-native/native-geocoder';
+
 @NgModule({
   declarations: [
     MyApp,
@@ -32,6 +35,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     TabsPage
   ],
   providers: [
+    Geolocation,
+    NativeGeocoder,
     StatusBar,
     SplashScreen,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Geolocation } from '@ionic-native/geolocation';
+import { NativeGeocoder, NativeGeocoderReverseResult } from '@ionic-native/native-geocoder';
+
 
 @Component({
   selector: 'page-home',
@@ -7,8 +10,31 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  lon: number = 0.0;
+  lat: number = 0.0;
+  reverseResult: any;
 
+  constructor(public navCtrl: NavController,
+              public geolocation: Geolocation,
+              public nativeGeocoder: NativeGeocoder) {
+    let watch = this.geolocation.watchPosition();
+    watch.subscribe((data) => {
+      if (data && data.coords) {
+        this.lon = data.coords.longitude;
+        this.lat = data.coords.latitude;
+        this.nativeGeocoder.reverseGeocode(this.lat, this.lon)
+          .then((result: NativeGeocoderReverseResult) => {
+            if (result[0]) {
+              this.reverseResult = result[0];
+            } else {
+              this.reverseResult = result;
+            }
+          }).catch((error: any) => {
+            this.reverseResult = null;
+            console.error(error);
+          });
+      }
+    });
   }
 
 }
